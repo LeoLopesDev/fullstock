3b0a8202d61f2d81583793ffe5e7dc26f41b4bbe
Leonardo <leolopesquermes1997@gmail.com>
2025-06-21 19:47:41 -0300

    Implementando StockException para lançar exceção personalizada para o front

---
+++ src/main/java/com/fullstack/stock/Exception/StockException.java
@@ -0,0 +1,19 @@
+package com.fullstack.stock.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class StockException {
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+}
